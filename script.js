37b24d5c161c2388cfe4ef558d9ade560a0d0899
FabioJRR-Escola <rocha.junior.fabio@escola.pr.gov.br>
2026-05-25 20:04:05 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+function fecharTela() {
+
+    document.getElementById('telaInicial')
+        .style.display = 'none';
+}
+
+function irPara(secao) {
+
+    fecharTela();
+
+    setTimeout(() => {
+
+        document.getElementById(secao)
+            .scrollIntoView({
+
+                behavior: 'smooth'
+            });
+
+    }, 200);
+}
+
 console.log("Site carregado com sucesso!");
