98fe9ed5c6d0ca28f432e36d6bdbfb3ccf5cea01
FabioJRR-Escola <rocha.junior.fabio@escola.pr.gov.br>
2026-06-01 21:49:14 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+let tamanhoFonte = 100;
+
 function fecharTela() {
 
-    document.getElementById('telaInicial')
-        .style.display = 'none';
+    document.getElementById("telaInicial")
+        .style.display = "none";
 }
 
 function irPara(secao) {
@@ -12,11 +14,37 @@ function irPara(secao) {
 
         document.getElementById(secao)
             .scrollIntoView({
-
-                behavior: 'smooth'
+                behavior: "smooth"
             });
 
     }, 200);
+}
+
+function alternarModoEscuro() {
+
+    document.body.classList.toggle("dark-mode");
+}
+
+function aumentarFonte() {
+
+    if (tamanhoFonte < 140) {
+
+        tamanhoFonte += 10;
+
+        document.body.style.fontSize =
+            tamanhoFonte + "%";
+    }
+}
+
+function diminuirFonte() {
+
+    if (tamanhoFonte > 80) {
+
+        tamanhoFonte -= 10;
+
+        document.body.style.fontSize =
+            tamanhoFonte + "%";
+    }
 }
 
 console.log("Site carregado com sucesso!");
